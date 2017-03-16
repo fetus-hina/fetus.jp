86be8d109ed5f0a6e6bb8c0af997953f022816da
AIZAWA Hina <hina@bouhime.com>
2017-03-16 23:11:34 +0900

    use generated cookie-secret file
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- webapp/config/web.php
+++ webapp/config/web.php
@@ -9,8 +9,7 @@ $config = [
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'ETiKyxPZUMe3z43POOOB9ZkpQVMAQugz',
+            'cookieValidationKey' => include(__DIR__ . '/cookie-secret.php'),
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
