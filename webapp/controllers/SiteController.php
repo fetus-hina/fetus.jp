9a6ab67e369ca9b22413ed18344ac39305e1913e
AIZAWA Hina <hina@fetus.jp>
2021-05-07 18:18:11 +0900

    Add site/clear-opcache action

--- webapp/controllers/SiteController.php
+++ webapp/controllers/SiteController.php
@@ -5,11 +5,34 @@ declare(strict_types=1);
 namespace app\controllers;
 
 use Yii;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\ErrorAction;
+use yii\web\Response;
 
 class SiteController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => [
+                    'clear-opcache',
+                ],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'ips' => [
+                            '127.0.0.0/8',
+                            '::1',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function actions()
     {
         return [
@@ -22,5 +45,20 @@ class SiteController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionClearOpcache(): string
+    {
+        $r = Yii::$app->response;
+        $r->format = Response::FORMAT_RAW;
+        $r->headers->set('Content-Type', 'text/plain; charset=UTF-8');
+
+        if (function_exists('opcache_reset')) {
+            opcache_reset();
+            return 'ok';
+        }
+
+        $r->statusCode = 501;
+        return 'not ok';
     }
 }
